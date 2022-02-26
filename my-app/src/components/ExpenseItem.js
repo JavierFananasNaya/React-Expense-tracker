c5a91582cbfc0581276667a784e07d15f9cc2707
JavierFananasNaya <javierfananasnaya@gmail.com>
2022-02-26 22:47:44 +0100

    simple adding styling

--- my-app/src/components/ExpenseItem.js
+++ my-app/src/components/ExpenseItem.js
@@ -1,5 +1,15 @@
+import './ExpenseItem.css'
+
 function ExpenseItem() {
-    return <h2>Expense Item!</h2>
+  return (
+    <div className="expense-item">
+      <div>11/09/2022</div>
+      <div className="expense-item__description">
+        <h2>Car insurance</h2>
+        <div className="expense-item__price">134€</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
