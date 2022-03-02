d37a7ab3e572b28c1d3534cefaa00fb3e091a2e0
JavierFananasNaya <javierfananasnaya@gmail.com>
2022-03-02 23:50:11 +0100

    delete form inputs value on submitting

--- my-app/src/components/NewExpense/form/ExpenseForm.js
+++ my-app/src/components/NewExpense/form/ExpenseForm.js
@@ -52,6 +52,12 @@ const ExpenseForm = () => {
       date: new Date(enteredDate)
     };
     console.log(expenseData);
+    
+    // after saving their state we stablish them to empty again here
+
+    setenteredTitle('');
+    setenteredDate('');
+    setenteredAmount('');
   };
 
   return (
@@ -59,7 +65,7 @@ const ExpenseForm = () => {
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler}></input>
+          <input type="text" value={enteredTitle} onChange={titleChangeHandler}></input>
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
@@ -67,6 +73,7 @@ const ExpenseForm = () => {
             type="number"
             min="0.01"
             step="0.01"
+            value={enteredAmount}
             onChange={amountChangeHandler}
           ></input>
         </div>
@@ -76,6 +83,7 @@ const ExpenseForm = () => {
             type="date"
             min="2019-01-01"
             max="2022-12-31"
+            value={enteredDate}
             onChange={dateChangeHandler}
           ></input>
         </div>
