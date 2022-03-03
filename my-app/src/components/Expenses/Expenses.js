b523b94e72af61a65ea1fb730d7a3640df6c4827
JavierFananasNaya <javierfananasnaya@gmail.com>
2022-03-03 22:57:32 +0100

    extracting html content to a variable

--- my-app/src/components/Expenses/Expenses.js
+++ my-app/src/components/Expenses/Expenses.js
@@ -11,8 +11,22 @@ function Expenses(props) {
     setSelectedYear(selectedYearOnFilter);
   };
 
-  const checkExpenseYear = (expense) => {
-    return expense.date.getFullYear() === selectedYear;
+  const filteredExpenses = props.items.filter(expense => {
+    return expense.date.getFullYear().toString() === selectedYear;
+  });
+
+
+  let expensesContent = <p>No expenses found.</p>
+
+  if (filteredExpenses.length > 0){
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      ></ExpenseItem>
+    ))
   }
 
   return (
@@ -22,14 +36,7 @@ function Expenses(props) {
           onSelectYear={filterChangedHandler}
           selected={selectedYear}
         ></ExpensesFilter>
-        {props.items.filter(checkExpenseYear).map((expense) => (
-            <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-            ></ExpenseItem>
-        ))}
+        {expensesContent}
       </Card>
     </div>
   );
