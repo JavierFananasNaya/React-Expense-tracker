63a1b146807b0a6109de8efc42bb8a43670cd842
JavierFananasNaya <javierfananasnaya@gmail.com>
2022-03-03 21:36:45 +0100

    adding items to dynamically render a list

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,22 +1,28 @@
+import React, { useState } from "react";
 import "./App.css";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 import HookExample from "./components/HookExample/HookExample";
 
-function App() {
-  const expenses = [
-    { id: "e2", title: "SuperMarket", amount: 23, date: new Date(2022, 1, 26) },
-    { id: "e3", title: "Car Fixing", amount: 222, date: new Date(2022, 2, 23) },
-    {
-      id: "e1",
-      title: "Car Ensurance",
-      amount: 123,
-      date: new Date(2022, 6, 26),
-    },
-    { id: "e4", title: "Coffee", amount: 1, date: new Date(2022, 8, 12) },
-  ];
+const DUMMY_EXPENSES = [
+  { id: "e2", title: "SuperMarket", amount: 23, date: new Date(2022, 1, 26) },
+  { id: "e3", title: "Car Fixing", amount: 222, date: new Date(2022, 2, 23) },
+  {
+    id: "e1",
+    title: "Car Ensurance",
+    amount: 123,
+    date: new Date(2022, 6, 26),
+  },
+  { id: "e4", title: "Coffee", amount: 1, date: new Date(2022, 8, 12) },
+];
 
+function App() {
+
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
   const addExpenseHandler = (expense) => {
+    setExpenses(prevExpenses => {
+      return [expense, ...prevExpenses];
+    });
     console.log(expense);
   };
 
